7419fa5702eb0e2393c44d9ff3d9f3173cfc315e
yash91989201 <yashraj6370@gmail.com>
2025-01-01 19:39:01 +0530

    fix: change location storage columns in user delivery address table
    remove one location column and add longitude and latitude column to
    store user's delivery address map location and location column to
    autogenerated column with POINT type

--- services/user/db/migrations/000002_delivery_address.up.sql
+++ services/user/db/migrations/000002_delivery_address.up.sql
@@ -4,7 +4,9 @@ CREATE TABLE IF NOT EXISTS `delivery_address`(
   `receiver_phone` VARCHAR(10) NOT NULL,
   `address_alias` ENUM("home", "work", "hotel", "other") DEFAULT("home") NOT NULL,
   `other_alias` VARCHAR(36) DEFAULT NULL,
-  `location` POINT NOT NULL,
+  `longitude` DOUBLE NOT NULL,
+  `latitude` DOUBLE NOT NULL,
+   location POINT NOT NULL,
   `address` VARCHAR(512) NOT NULL,
   `nearby_landmark` VARCHAR(256) DEFAULT NULL,
   `delivery_instruction` VARCHAR(256) DEFAULT NULL,
@@ -12,3 +14,20 @@ CREATE TABLE IF NOT EXISTS `delivery_address`(
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 );
+
+-- Trigger to update location on insert
+CREATE TRIGGER insert_location
+BEFORE INSERT ON `delivery_address` 
+FOR EACH ROW
+BEGIN
+  SET NEW.location = ST_GeomFromText(CONCAT('POINT(', NEW.longitude, ' ', NEW.latitude, ')'));
+END;
+
+-- Trigger to update location on update
+CREATE TRIGGER update_location
+BEFORE UPDATE ON `delivery_address` 
+FOR EACH ROW
+BEGIN
+  SET NEW.location = ST_GeomFromText(CONCAT('POINT(', NEW.longitude, ' ', NEW.latitude, ')'));
+END;
+
